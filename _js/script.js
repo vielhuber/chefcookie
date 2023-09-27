6b82e0ef84c95655066c1bc9e0b8b2b2759bad1b
David Vielhuber <david@vielhuber.de>
2023-09-27 12:53:10 +0200

    Upgrade linkedin insights tag.

--- _js/script.js
+++ _js/script.js
@@ -1773,11 +1773,13 @@ export default class chefcookie {
             document.body.appendChild(script);
             script = document.createElement('script');
             script.innerHTML = `
-                (function(){var s = document.getElementsByTagName("script")[0];
+                (function(l) {if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                window.lintrk.q=[]}
+                var s = document.getElementsByTagName("script")[0];
                 var b = document.createElement("script");
                 b.type = "text/javascript";b.async = true;
                 b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-                s.parentNode.insertBefore(b, s);})();
+                s.parentNode.insertBefore(b, s);})(window.lintrk);
             `;
             document.body.appendChild(script);
             script = document.createElement('noscript');
