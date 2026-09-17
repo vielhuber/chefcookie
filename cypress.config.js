0db9f9b65bc34577e109183f1e49b035f9eae8ef
David Vielhuber <david@vielhuber.de>
2026-09-17 13:51:47 +0200

    - Remove the Cypress environment variable restriction. - Bump the lockfile package version to 3.1.1.

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 let { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-    allowCypressEnv: false,
     e2e: {
         baseUrl: 'http://localhost:18080',
         specPattern: 'cypress/integration/**/*.js',
